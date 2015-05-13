43d80711498b1a8fdf52d984dd5aed40cec5ef92
WalkingFood <ants.basiceros@gmail.com>
2015-05-13 12:19:05 -0400

    Writing simple weather request method using camel

--- src/main/java/com/walkingfood/bootstrap/CamelRoutes.java
+++ src/main/java/com/walkingfood/bootstrap/CamelRoutes.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Component;
 /**
  * Created by Andrew Fooden on 5/13/2015.
  * Part of SpringBootTemplate.
+ * Modified in WeatherRequester.
  */
 @Component
 public class CamelRoutes extends SpringRouteBuilder {
@@ -19,5 +20,11 @@ public class CamelRoutes extends SpringRouteBuilder {
         //Building a route from properties extracted from application.yml
         from("{{routes.from.start}}")
                 .to("{{routes.to.finish}}");
+
+
+        //For getting information from the Camel Weather endpoint
+        from("seda:request/weather")
+                .to("weather:foo?location=Madrid,Spain")
+                .to("log:com.walkingfood?showAll=true");
     }
 }
--- src/main/java/com/walkingfood/weather/WeatherRequest.java
+++ src/main/java/com/walkingfood/weather/WeatherRequest.java
@@ -1,8 +1,59 @@
 package com.walkingfood.weather;
 
+import org.apache.camel.EndpointInject;
+import org.apache.camel.ProducerTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
 /**
  * Created by Andrew Fooden on 5/13/2015.
  * Part of WeatherRequester.
  */
+@Service
 public class WeatherRequest {
+
+    @Value("$(weather.apiKey)")
+    String apiKey;
+
+    @EndpointInject
+    ProducerTemplate template;
+
+    private static final String ROUTE = "seda:request/weather";
+
+    private static final String HEADER_NAME = "CamelWeatherLocation";
+    private static final String EMPTY_BODY = "";
+
+    @Scheduled(initialDelay = 100, fixedDelay = 20000)
+    public void requestWeather(){
+        requestWeather(null);
+    }
+
+    public void requestWeather(String location){
+
+        StringBuilder stringBuilder = new StringBuilder();
+
+        if (location != null) {
+            stringBuilder.append(location);
+        }
+
+        if (!apiKey.isEmpty() && !apiKey.equalsIgnoreCase("test")){
+            if (stringBuilder.length() > 1){
+                stringBuilder.append("&");
+                stringBuilder.append(apiKey);
+            }
+        }
+
+        if (stringBuilder.length() > 1) {
+            template.requestBodyAndHeader(
+                    ROUTE,
+                    EMPTY_BODY,
+                    HEADER_NAME,
+                    stringBuilder,
+                    String.class);
+        }
+        else {
+            template.requestBody(ROUTE, EMPTY_BODY);
+        }
+    }
 }
