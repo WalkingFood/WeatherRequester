3ab41edd6b32a8abd8652fddaeb8ced1a0c80962
WalkingFood <ants.basiceros@gmail.com>
2015-05-13 10:41:30 -0400

    Adding in simple route tests using scheduled annotation

--- src/main/java/com/walkingfood/bootstrap/CamelRoutes.java
+++ src/main/java/com/walkingfood/bootstrap/CamelRoutes.java
@@ -1,16 +1,23 @@
 package com.walkingfood.bootstrap;
 
 import org.apache.camel.spring.SpringRouteBuilder;
+import org.springframework.stereotype.Component;
 
 /**
  * Created by Andrew Fooden on 5/13/2015.
  * Part of SpringBootTemplate.
  */
+@Component
 public class CamelRoutes extends SpringRouteBuilder {
 
     @Override
     public void configure() throws Exception {
-        from("{{routes.from.x}}")
-                .to("{{routes.to.y}}");
+
+        //Building a route from explicitly declared Strings
+        from("seda:beginning").to("seda:end").end();
+
+        //Building a route from properties extracted from application.yml
+        from("{{routes.from.start}}")
+                .to("{{routes.to.finish}}");
     }
 }
---
+++ src/main/java/com/walkingfood/test/TestService.java
@@ -0,0 +1,35 @@
+package com.walkingfood.test;
+
+import org.apache.camel.Consume;
+import org.apache.camel.EndpointInject;
+import org.apache.camel.ProducerTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by Andrew Fooden on 5/13/2015.
+ * Part of SpringBootTemplate.
+ */
+@Service
+public class TestService {
+
+    @EndpointInject
+    ProducerTemplate template;
+
+    @Scheduled(initialDelay = 100, fixedDelay = 2000)
+    public void runTest(){
+        System.err.println("Sending messages");
+        template.sendBody("seda:start", "Plain route test worked");
+        template.sendBody("seda:beginning", "Properties-set route test worked");
+    }
+
+    @Consume(uri = "seda:finish")
+    public void consumeTest1(String message){
+        System.err.println(message);
+    }
+
+    @Consume(uri = "seda:end")
+    public void consumeTest2(String message){
+        System.err.println(message);
+    }
+}
