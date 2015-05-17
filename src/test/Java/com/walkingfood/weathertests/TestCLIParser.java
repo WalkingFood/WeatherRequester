c80f8853b1b02e61659de8d3d5c3084da96ca6e7
WalkingFood <ants.basiceros@gmail.com>
2015-05-17 10:09:31 -0400

    Fleshing out CLI parser tests

--- src/test/Java/com/walkingfood/weathertests/TestCLIParser.java
+++ src/test/Java/com/walkingfood/weathertests/TestCLIParser.java
@@ -2,6 +2,8 @@ package com.walkingfood.weathertests;
 
 import com.beust.jcommander.ParameterException;
 import com.walkingfood.weather.cli.DoubleValidator;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 
@@ -14,16 +16,39 @@ import static org.springframework.test.util.AssertionErrors.fail;
  */
 public class TestCLIParser {
 
+    DoubleValidator validator = new DoubleValidator();
+
+    @Before
+    public void setUp(){}
+
     @Test
     public void testDoubleValidatorPassing(){
-        DoubleValidator validator = new DoubleValidator();
-        validator.validate("-lat","30.423d");
+        validator.validate("-lat","30.423");
+    }
+
+    @Test
+    public void testIntegerValidatorPassing(){
+        validator.validate("-lat", "66");
     }
 
     @Test(expected = ParameterException.class)
     public void testEmptyStringValidationFail(){
-        DoubleValidator validator = new DoubleValidator();
         validator.validate("-lat","");
         fail("Should not complete with empty string");
     }
+
+    @Test(expected = ParameterException.class)
+    public void testMalformedDoubleValidationFail(){
+        validator.validate("-lat","42.4f3");
+        fail("Should not complete with malformed string");
+    }
+
+    @Test(expected = ParameterException.class)
+    public void testMalformedDoubleValidationFail2(){
+        validator.validate("-lat","42. 43");
+        fail("Should not complete with malformed string");
+    }
+
+    @After
+    public void tearDown(){}
 }
