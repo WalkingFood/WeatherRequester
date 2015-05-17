8d46ba8d26181120d8525a861516cd41a8647835
WalkingFood <ants.basiceros@gmail.com>
2015-05-17 09:33:50 -0400

    Starting to lay out some test framework

---
+++ src/test/Java/com/walkingfood/weathertests/TestWeatherRequest.java
@@ -0,0 +1,26 @@
+package com.walkingfood.weathertests;
+
+import com.walkingfood.bootstrap.Application;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * Created by Andrew Fooden on 5/17/2015.
+ * Part of WeatherRequester.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Application.class)
+public class TestWeatherRequest {
+
+    @Test
+    void testCamelConnectivity(){
+
+    }
+
+    @Test
+    void testWeatherMessage(){
+
+    }
+}
